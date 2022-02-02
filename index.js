73fcf3462a5cbc3dddc3842b67681f5499c3d3fb
RvInfo Sistemas <ricardovieirasp@outlook.com>
2022-02-02 19:43:32 -0300

    002

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ class Cliente{
 }
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo=0;
 
     sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
             console.log("saque em " + this.agencia + " no valor de " + valor);
+            return valor;
         }
     }
 
     depositar(valor) {
         if(valor>0) {
-            this.saldo += valor;
+            this._saldo += valor;
             console.log("deposito em " + this.agencia + " no valor de " + valor);
+            return valor;
         }
     }
 }
@@ -30,12 +32,9 @@ cliente2.nome = "roseli";
 cliente2.cpf = "555666444-55";
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 2553;
 
 console.log(cliente1);
-console.log(contaCorrenteRicardo);
-
 console.log(cliente2);
 
 console.log(contaCorrenteRicardo);
